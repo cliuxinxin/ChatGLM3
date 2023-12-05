2fc1d61d668b7d892f3242291287b3ef9392ee99
liuxinxin <cliuxinxin@gmail.com>
2023-12-05 17:09:47 +0800

    增加列表

--- my_demo/tool_registry.py
+++ my_demo/tool_registry.py
@@ -153,7 +153,7 @@ def search_knowledge_base(
         similar_documents = db.similarity_search_by_vector(query_embedding, k=4)  # k指定返回的相似文档数量
 
         # 组合相似文档的内容
-        return '\n\n'.join([doc.page_content for doc in similar_documents])
+        return '\n\n'.join([f'第 {index + 1} 段: {doc.page_content}' for index, doc in enumerate(similar_documents)])
     except Exception as e:
         return f"Error encountered while searching the knowledge base: {str(e)}"
 
